4c58b0d8696af9da45807ac40511843c8a39dc1e
pericyan <addarrow3@gmail.com>
2024-08-12 22:27:52 +0900

    문의하기 페이지 수정

--- WarmBoysApp/lib/screens/service/write_inquiry_screen.dart
+++ WarmBoysApp/lib/screens/service/write_inquiry_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_email_sender/flutter_email_sender.dart';
+import 'package:flutter/services.dart';
 
 class WriteInquiryScreen extends StatefulWidget {
   final String uid;
@@ -14,7 +15,7 @@ class _WriteInquiryScreenState extends State<WriteInquiryScreen> {
   String? _selectedCategory;
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _contentController = TextEditingController();
-  bool _isButtonEnabled = false; // 버튼 활성화 상태를 관리할 변수
+  bool _isButtonEnabled = false;
 
   final List<String> _inquiryCategories = [
     '매칭 관련 일반 문의',
@@ -29,12 +30,10 @@ class _WriteInquiryScreenState extends State<WriteInquiryScreen> {
   void initState() {
     super.initState();
 
-    // 모든 텍스트 필드의 리스너에 _onTextFieldChanged 메서드를 연결
     _titleController.addListener(_onTextFieldChanged);
     _contentController.addListener(_onTextFieldChanged);
   }
 
-  // 사용자가 텍스트 필드에 입력할 때마다 호출
   void _onTextFieldChanged() {
     setState(() {
       _isButtonEnabled = _selectedCategory != null &&
@@ -45,13 +44,12 @@ class _WriteInquiryScreenState extends State<WriteInquiryScreen> {
 
   Future<void> _sendEmail() async {
     final String subject = '[${_selectedCategory}] ${_titleController.text}';
-    final String body =
-        'uid: ${widget.uid}\n\n${_contentController.text}'; // 본문 첫 줄에 uid 추가
+    final String body = 'uid: ${widget.uid}\n\n${_contentController.text}';
 
     final Email emailToSend = Email(
       body: body,
       subject: subject,
-      recipients: ['addarrow3@gmail.com'], // 수신자 이메일 주소
+      recipients: ['addarrow3@gmail.com'],
       isHTML: false,
     );
 
@@ -60,7 +58,12 @@ class _WriteInquiryScreenState extends State<WriteInquiryScreen> {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('문의가 접수되었습니다.')),
       );
-      _resetForm(); // 폼을 초기화
+
+      // 키보드 내려가게 하기
+      FocusScope.of(context).unfocus();
+      SystemChannels.textInput.invokeMethod('TextInput.hide');
+
+      _resetForm();
     } catch (error) {
       print('Email send error: $error');
       ScaffoldMessenger.of(context).showSnackBar(
@@ -69,19 +72,17 @@ class _WriteInquiryScreenState extends State<WriteInquiryScreen> {
     }
   }
 
-  // 폼을 초기화하는 메서드
   void _resetForm() {
     setState(() {
       _selectedCategory = null;
       _titleController.clear();
       _contentController.clear();
-      _isButtonEnabled = false; // 버튼 비활성화
+      _isButtonEnabled = false;
     });
   }
 
   @override
   void dispose() {
-    // 리스너 제거
     _titleController.dispose();
     _contentController.dispose();
     super.dispose();
@@ -132,7 +133,7 @@ class _WriteInquiryScreenState extends State<WriteInquiryScreen> {
                       setState(() {
                         _selectedCategory = newValue;
                       });
-                      _onTextFieldChanged(); // 문의 분류 선택 시 버튼 상태 업데이트
+                      _onTextFieldChanged();
                     },
                     items: _inquiryCategories.map((category) {
                       return DropdownMenuItem<String>(
@@ -210,13 +211,9 @@ class _WriteInquiryScreenState extends State<WriteInquiryScreen> {
                   ),
                   SizedBox(height: 30),
                   SizedBox(
-                    width: double.infinity, // 가로로 확장
+                    width: double.infinity,
                     child: ElevatedButton(
-                      onPressed: _isButtonEnabled
-                          ? () {
-                              _sendEmail(); // 문의 접수 버튼을 눌렀을 때 이메일 전송
-                            }
-                          : null, // 비활성화
+                      onPressed: _isButtonEnabled ? _sendEmail : null,
                       style: ElevatedButton.styleFrom(
                         padding: EdgeInsets.symmetric(vertical: 16),
                         backgroundColor: _isButtonEnabled
