c19cf52c9754265b5c8d2f588bf1a6873e5272b2
pericyan <addarrow3@gmail.com>
2024-08-13 20:31:32 +0900

    시니어) 내 프로필

---
+++ WarmBoysApp/lib/screens/profile/profile_senior_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import '../../providers/custom_auth_provider.dart';
+
+class ProfileSeniorScreen extends StatelessWidget {
+  const ProfileSeniorScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
