51d7b7480294d07cf39d34cf4111a3274ebb454d
Jung solbin <balbalbin@naver.com>
2024-08-14 16:49:12 +0900

    알림 수정

--- WarmBoysApp/lib/screens/notification/notification_page.dart
+++ WarmBoysApp/lib/screens/notification/notification_page.dart
@@ -3,8 +3,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
 import '/../providers/custom_auth_provider.dart';
+import '../index/main_index.dart';
 import '../chatting/chat_screen.dart';
-import '../main/matching_screen.dart';
 
 class NotificationPage extends StatefulWidget {
   @override
@@ -12,8 +12,6 @@ class NotificationPage extends StatefulWidget {
 }
 
 class _NotificationPageState extends State<NotificationPage> {
-  String _selectedCategory = '공고 알림';
-
   @override
   Widget build(BuildContext context) {
     final userId = Provider.of<CustomAuthProvider>(context, listen: false).uid;
@@ -22,6 +20,7 @@ class _NotificationPageState extends State<NotificationPage> {
       return Scaffold(
         appBar: AppBar(
           title: Text('알림'),
+          backgroundColor: Colors.grey, // 상단 타일 배경색을 옅은 회색으로 설정
         ),
         body: Center(
           child: Text('로그인이 필요합니다.'),
@@ -32,106 +31,89 @@ class _NotificationPageState extends State<NotificationPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('알림'),
+        backgroundColor: Colors.grey[200], // 상단 타일 배경색을 옅은 회색으로 설정
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: DropdownButton<String>(
-              value: _selectedCategory,
-              onChanged: (String? newValue) {
-                setState(() {
-                  _selectedCategory = newValue!;
-                });
-              },
-              items: <String>['공고 알림', '채팅 알림']
-                  .map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-          ),
-          Expanded(
-            child: StreamBuilder<QuerySnapshot>(
-              stream: FirebaseFirestore.instance
-                  .collection('alarms')
-                  .doc(userId)
-                  .collection('userAlarms')
-                  .where('alarmType', isEqualTo: _selectedCategory == '공고 알림' ? 'post' : 'chat')
-                  .orderBy('timestamp', descending: true)
-                  .snapshots(),
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return Center(child: CircularProgressIndicator());
-                }
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('alarms')
+            .doc(userId)
+            .collection('userAlarms')
+            .orderBy('timestamp', descending: true)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(child: CircularProgressIndicator());
+          }
 
-                var alarms = snapshot.data!.docs;
+          var alarms = snapshot.data!.docs;
 
-                if (alarms.isEmpty) {
-                  return Center(child: Text('새로운 알림이 없습니다.'));
-                }
+          if (alarms.isEmpty) {
+            return Center(child: Text('새로운 알림이 없습니다.'));
+          }
 
-                return ListView.builder(
-                  itemCount: alarms.length,
-                  itemBuilder: (context, index) {
-                    var alarm = alarms[index].data() as Map<String, dynamic>;
-                    var alarmId = alarms[index].id;
+          return ListView.separated(
+            itemCount: alarms.length,
+            separatorBuilder: (context, index) => Divider(color: Colors.grey), // 회색 구분선 추가
+            itemBuilder: (context, index) {
+              var alarm = alarms[index].data() as Map<String, dynamic>;
+              var alarmId = alarms[index].id;
+              bool isRead = alarm['read'] ?? true; // read 상태를 확인, 기본값은 true
 
-                    return ListTile(
-                      title: Text(alarm['message']),
-                      subtitle: Text(
-                        DateFormat('yyyy-MM-dd HH:mm')
-                            .format(alarm['timestamp'].toDate()),
-                      ),
-                      trailing: IconButton(
-                        icon: Icon(Icons.close),
-                        onPressed: () async {
-                          await FirebaseFirestore.instance
-                              .collection('alarms')
-                              .doc(userId)
-                              .collection('userAlarms')
-                              .doc(alarmId)
-                              .update({'read': true});
-                          setState(() {});
-                        },
-                      ),
-                      onTap: () async {
-                        await FirebaseFirestore.instance
-                            .collection('alarms')
-                            .doc(userId)
-                            .collection('userAlarms')
-                            .doc(alarmId)
-                            .update({'read': true});
+              return Container(
+                color: isRead ? Colors.transparent : Colors.grey[200], // 읽지 않은 알림의 배경을 옅은 회색으로 설정
+                child: ListTile(
+                  title: Text(alarm['message']),
+                  subtitle: Text(
+                    DateFormat('yyyy-MM-dd HH:mm')
+                        .format(alarm['timestamp'].toDate()),
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(Icons.close, color: Colors.red), // 우측 상단에 X 아이콘 추가
+                    onPressed: () async {
+                      // Firestore에서 알림 삭제
+                      await FirebaseFirestore.instance
+                          .collection('alarms')
+                          .doc(userId)
+                          .collection('userAlarms')
+                          .doc(alarmId)
+                          .delete();
 
-                        if (_selectedCategory == '채팅 알림') {
-                          // 채팅 알림일 경우 채팅 화면으로 이동
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => ChatScreen(
-                                chatId: alarm['chatId'],
-                              ),
-                            ),
-                          );
-                        } else if (_selectedCategory == '공고 알림') {
-                          // 공고 알림일 경우 매칭 화면으로 이동
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => MatchingScreen(),
-                            ),
-                          );
-                        }
-                      },
-                    );
+                      // 알림이 삭제되었다는 스낵바 메시지
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('알림이 삭제되었습니다.')),
+                      );
+                    },
+                  ),
+                  onTap: () async {
+                    // 알림 읽음으로 표시
+                    await FirebaseFirestore.instance
+                        .collection('alarms')
+                        .doc(userId)
+                        .collection('userAlarms')
+                        .doc(alarmId)
+                        .update({'read': true});
+
+                    if (alarm['alarmType'] == 'chat') {
+                      // 채팅 알림일 경우 특정 채팅방으로 이동
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ChatScreen(
+                            chatId: alarm['chatId'],
+                          ),
+                        ),
+                      );
+                    } else if (alarm['alarmType'] == 'post') {
+                      // 공고 알림일 경우 MainIndex의 매칭 화면으로 이동
+                      MainIndex.globalKey.currentState?.navigateToMatchingScreen();
+                      Navigator.pop(context); // 알림 페이지에서 벗어나기 위해 pop 호출
+                    }
                   },
-                );
-              },
-            ),
-          ),
-        ],
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
--- WarmBoysApp/lib/widgets/custum_alarm.dart
+++ WarmBoysApp/lib/widgets/custum_alarm.dart
@@ -4,37 +4,33 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import '../providers/custom_auth_provider.dart';
 import '../screens/notification/notification_page.dart';
 
-class CustomAlarmButton extends StatefulWidget {
-  @override
-  _CustomAlarmButtonState createState() => _CustomAlarmButtonState();
-}
-
-class _CustomAlarmButtonState extends State<CustomAlarmButton> {
-  bool hasNewAlarms = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _checkForAlarms();
-  }
-
-  Future<void> _checkForAlarms() async {
-    final userId = Provider.of<CustomAuthProvider>(context, listen: false).uid;
-    if (userId != null) {
-      final alarmSnapshot = await FirebaseFirestore.instance
-          .collection('alarms')
-          .doc(userId)
-          .get();
-      if (alarmSnapshot.exists && alarmSnapshot.data()?['hasNewAlarms'] == true) {
-        setState(() {
-          hasNewAlarms = true;
-        });
-      }
-    }
-  }
-
+class CustomAlarmButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final userId = Provider.of<CustomAuthProvider>(context, listen: false).uid;
+
+    if (userId == null) {
+      return _buildIconButton(context, false);
+    }
+
+    return StreamBuilder<DocumentSnapshot>(
+      stream: FirebaseFirestore.instance
+          .collection('alarms')
+          .doc(userId)
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData || !snapshot.data!.exists) {
+          return _buildIconButton(context, false);
+        }
+
+        final data = snapshot.data!.data() as Map<String, dynamic>;
+        final hasNewAlarms = data['hasNewAlarms'] == true;
+        return _buildIconButton(context, hasNewAlarms);
+      },
+    );
+  }
+
+  Widget _buildIconButton(BuildContext context, bool hasNewAlarms) {
     return Stack(
       children: [
         IconButton(
