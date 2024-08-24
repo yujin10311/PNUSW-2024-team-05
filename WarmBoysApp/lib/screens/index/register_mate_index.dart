a650a6b3b30ee4c41e34482de11b8232d687329d
pericyan <addarrow3@gmail.com>
2024-08-25 04:24:04 +0900

    register_mate_index 수정

--- WarmBoysApp/lib/screens/index/register_mate_index.dart
+++ WarmBoysApp/lib/screens/index/register_mate_index.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../register/register_mate_screen_1.dart';
 import '../register/register_mate_screen_2.dart';
 import '../register/register_mate_screen_3.dart';
 import '../register/register_mate_screen_4.dart';
 import '../register/register_mate_screen_5.dart';
+import '../register/register_mate_screen_6.dart';
 import '../register/register_emailpassword_screen_6.dart';
 import '../../utils/shared_preferences_helper.dart';
+import '../../providers/custom_auth_provider.dart';
 
 class RegisterMateIndex extends StatefulWidget {
   @override
@@ -25,6 +28,7 @@ class _RegisterMateIndexState extends State<RegisterMateIndex> {
       RegisterMateScreen3(onNextPage: _nextPage, onPreviousPage: _previousPage),
       RegisterMateScreen4(onNextPage: _nextPage, onPreviousPage: _previousPage),
       RegisterMateScreen5(onNextPage: _nextPage, onPreviousPage: _previousPage),
+      RegisterMateScreen6(onNextPage: _nextPage, onPreviousPage: _previousPage),
       RegisterEmailpasswordScreen6(
           onNextPage: _nextPage, onPreviousPage: _previousPage),
     ]);
@@ -41,13 +45,17 @@ class _RegisterMateIndexState extends State<RegisterMateIndex> {
   }
 
   void _previousPage() async {
+    final customAuthProvider =
+        Provider.of<CustomAuthProvider>(context, listen: false);
     if (_currentIndex > 0) {
       setState(() {
         _currentIndex--;
       });
     } else {
       await SharedPreferencesHelper.clearAll();
-      print("SharedPreference 초기화 완료");
+      customAuthProvider.clearProfileImage();
+      customAuthProvider.clearSchoolCertImage();
+      print("이미지, SharedPreference 초기화 완료");
       Navigator.pop(context);
     }
   }
